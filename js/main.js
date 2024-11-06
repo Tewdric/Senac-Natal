42d802e62f82318401f7ac5469201b26e572e363
Tewdric <manoelvictor756@gmail.com>
2024-11-05 22:33:12 -0300

    Adicionei função para contabilizar os dias para o natal, meudei a estrutura do site, coloquei uma sidebar, coloquei os espaços ondem devem serem preenchidos os campos

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const evento = document.getElementById('evento')
+const contador = document.createElement('h1')
+
+// Defina a data final da contagem regressiva (substitua com a sua data)
+const dataFinal = new Date('2024-12-25 23:59:59').getTime();
+
+// Função para atualizar o contador a cada segundo
+function atualizarContador() {
+
+    const agora = new Date().getTime();
+    const diferenca = dataFinal - agora;
+
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+
+    contador.innerHTML=`${dias}d ${horas}h ${minutos}m ${segundos}s`
+    if (diferenca <= 0) {
+        clearInterval(interval);
+        console.log("Contagem encerrada!");
+    }
+}
+
+evento.appendChild(contador)
+// Chame a função para iniciar a contagem regressiva e atualizar a cada segundo
+const interval = setInterval(atualizarContador, 1000);
