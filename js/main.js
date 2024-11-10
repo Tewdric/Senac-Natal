c947d43cfee9826fe19d13f345ba9d8082fbf4a4
Tewdric <manoelvictor756@gmail.com>
2024-11-10 15:27:06 -0300

    Criado o sistema de inscrição funcional

--- js/main.js
+++ js/main.js
@@ -26,4 +26,36 @@ evento.appendChild(contador)
 // Chame a função para iniciar a contagem regressiva e atualizar a cada segundo
 const interval = setInterval(atualizarContador, 1000);
 
+// // Este código adiciona um evento de clique ao elemento com o id 'emailButton'
+// document.getElementById('emailButton').addEventListener('click', sendMail);
 
+// // Esta é a função que será chamada quando o botão com id 'emailButton' for clicado
+// function sendMail (e) {
+//     // Previne o comportamento padrão do clique em um botão dentro de um formulário.
+//     e.preventDefault();
+    
+//     // Obtém os valores dos campos de entrada do formulário com base em seus respectivos IDs.
+//     const mail = document.getElementById('email').value;
+//     const name = document.getElementById('name').value;
+
+//     // Verifica se todos os campos (e-mail, mensagem e nome) foram preenchidos pelo usuário.
+//     if (mail && name) {
+//         // Se todos os campos estiverem preenchidos, usa a biblioteca 'Email.js'
+//         // para enviar o e-mail com os detalhes fornecidos pelo usuário.
+
+//           Email.send({
+//             Host : "smtp.elasticemail.com",
+//             Username : "xhadayt@gmail.com",
+//             Password : "7B2F318ACA0C71EEF74635AE101144558CE20",
+//             To : 'manoelvictor756@gmail.com',
+//             From : "xhadayt@gmail.com",
+//             Subject : "This is the subject",
+//             Body : "And this is the body"
+//         }).then(
+//           message => alert(message)
+//         );
+//     } else {
+//         // Se algum dos campos estiver em branco, exibe um alerta informando que todos os campos devem ser preenchidos.
+//         alert('Preencha todos os campos!');
+//     }    
+// }
